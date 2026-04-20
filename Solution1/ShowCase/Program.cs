15aa6455dbea752e1ea0adbd3df3b847fbad593e
Nnaataalii <kiamova384@gmail.com>
2026-04-20 23:00:11 +0300

    kros4.1

---
+++ Solution1/ShowCase/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using UtilityLibraries;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int row = 0;
+
+        do
+        {
+            if (row == 0 || row >= 25)
+                row = ResetConsole(row);
+
+            string? input = Console.ReadLine();
+            if (string.IsNullOrEmpty(input)) break;
+
+            Console.WriteLine($"Input: {input}");
+            Console.WriteLine("Begins with uppercase? " +
+                $"{(input.StartsWithUpper() ? "Yes" : "No")}");
+            Console.WriteLine();
+
+            row += 4;
+
+        } while (true);
+    }
+
+    static int ResetConsole(int row)
+    {
+        if (row > 0)
+        {
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+        }
+
+        Console.Clear();
+
+        Console.WriteLine(
+            $"{Environment.NewLine}Press <Enter> only to exit; otherwise, enter a string and press <Enter>:{Environment.NewLine}"
+        );
+
+        return 3;
+    }
+}
